a26609210ca25562bb5ace4a5cb6044974c3a670
Claudio Ferreira <claudio.ferreira@aizoon.it>
2022-10-14 10:43:02 +0200

    after: add tests on storybook and mocks

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+
+import { SignIn } from './pages/SignIn';
 import './styles/global.css';
+
 
 export function App() {
 
   return (
-    <>
-      <h1 className='font-bold text-2xl text-violet-500'>Hello Mars</h1>
-    </>
+    <SignIn />
   )
 }
